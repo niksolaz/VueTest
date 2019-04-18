8b3badcc224c9652dc0ce18eccbbf2da623a21bd
Nico Solazzo <niksolaz1@MacBook-Pro-di-Nico.local>
2019-04-19 01:44:16 +0200

    add comp

--- vue-app-test/src/components/FirstComponent.vue
+++ vue-app-test/src/components/FirstComponent.vue
@@ -1,15 +1,47 @@
 <template>
+<v-container>
   <v-layout justify-center>
-    <h1 class="headline font-weight-bold mb-3"> 
-      Welcome on Vue app test
-    </h1>
+    <div>
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="50"
+        ></v-img>
+      </v-flex>
+      <v-flex xs12>
+        <div
+        text-xs-center
+        wrap
+        class="mb-5"
+        >
+          <h2>{{ myVar }}</h2>
+        </div>
+      </v-flex>
+
+      <v-flex xs12>
+        <v-btn @click="myVar = 'Hi, I am John Doe'">Name</v-btn>
+        <v-btn @click="myVar = 'this is my phone: +47 1234567890 '">Cell</v-btn>
+        <v-btn @click="myVar = 'this is my email: johndoe@info.com'">Email</v-btn>
+      </v-flex>
+    </div>
   </v-layout>
+</v-container>
 </template>
 
 <script>
   export default {
     name: 'FirstComponent',
+    data(){
+      return {
+        myVar:'',
+      }
+    },
   }
 </script>
 
 
+<style>
+  
+</style>
--- vue-app-test/src/components/HelloWorld.vue
+++ vue-app-test/src/components/HelloWorld.vue
@@ -1,18 +1,5 @@
 <template>
   <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
-    </v-layout>
     <v-layout justify-center>
       <h2 class="headline font-weight-bold mb-3">
       <a
